37d99290e98414bb8911cecd58abfc617661c06a
bella-tang <79435936+bella-tang@users.noreply.github.com>
2024-11-09 16:20:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,159 @@
+import polars as pl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from fpdf import FPDF
+
+
+def read_data():
+    df = pl.read_csv("data/spotify.csv")
+    df = df.with_columns((pl.col("duration_ms") / 1000).alias("duration_s"))
+    df = df.drop("duration_ms")
+    # print(df.head())
+    return df
+
+
+def calc_stats(df):
+    numerical_columns = [
+        "popularity",
+        "duration_s",
+        "explicit",
+        "danceability",
+        "energy",
+        "key",
+        "loudness",
+        "mode",
+        "speechiness",
+        "acousticness",
+        "instrumentalness",
+        "liveness",
+        "valence",
+        "tempo",
+        "time_signature",
+    ]
+
+    # initialize dict to store stats
+    stats_dict = {"column": [], "mean": [], "median": [], "std_dev": []}
+
+    # calculate stats for each column
+    for col in numerical_columns:
+        mean_value = round(df[col].mean(), 2)
+        median_value = round(df[col].median(), 2)
+        std_dev_value = round(df[col].std(), 2)
+
+        # store results to dict
+        stats_dict["column"].append(col)
+        stats_dict["mean"].append(mean_value)
+        stats_dict["median"].append(median_value)
+        stats_dict["std_dev"].append(std_dev_value)
+
+    # convert dict to polars df (table format)
+    stats_df = pl.DataFrame(stats_dict)
+    # convert to pandas df
+    stats_pd = stats_df.to_pandas()
+
+    # save df as image
+    fig, ax = plt.subplots(figsize=(12, len(stats_df) * 0.4))
+    ax.axis("tight")
+    ax.axis("off")
+    # create table
+    table = ax.table(
+        cellText=stats_pd.values,
+        colLabels=stats_pd.columns,
+        cellLoc="center",
+        loc="center",
+    )
+    table.auto_set_font_size(False)
+    table.set_fontsize(16)
+    table.scale(1.5, 2.5)
+
+    # save fig to folder
+    fig.savefig("resources/stats_df.png", bbox_inches="tight", dpi=300)
+    plt.close()
+    print(stats_df)
+    return stats_df
+
+
+def create_viz(df):
+    plt.figure(figsize=(8, 4))
+
+    # first subplot: histogram of duration
+    plt.subplot(1, 2, 1)
+    sns.histplot(df["duration_s"], bins=100, kde=True)
+    plt.title("Distribution of Track Duration")
+    plt.xlabel("Duration (in seconds)")
+    plt.xlim(0, 800)
+    plt.ylabel("Frequency")
+    plt.grid(True)
+
+    # second subplot: scatterplot of loudness vs energy
+    plt.subplot(1, 2, 2)
+    sns.scatterplot(x="loudness", y="energy", data=df, s=20, alpha=0.3)
+    plt.title("Song's Energy vs Volume")
+    plt.xlabel("Loudness (in dB)")
+    plt.ylabel("Energy")
+    plt.grid(True)
+
+    # save and output
+    plt.tight_layout()
+    plt.savefig("resources/plot.png", dpi=300, bbox_inches="tight")
+    plt.show()
+
+
+def create_report(csv_file):
+    # read data and get info
+    df = read_data()
+    dataset_name = os.path.basename(csv_file)
+    num_cols = df.width
+    col_names = df.columns
+
+    # generate stats and visualizations if not already saved
+    if not os.path.exists("plot.png"):
+        create_viz(df)
+    if not os.path.exists("stats_df.png"):
+        calc_stats(df)
+
+    # create empty PDF
+    pdf = FPDF()
+    pdf.set_auto_page_break(auto=True, margin=15)
+    pdf.add_page()
+
+    # title
+    pdf.set_font("Arial", "B", 16)
+    pdf.cell(200, 10, txt=f"Summary Report for {dataset_name}", ln=True, align="C")
+
+    # data summary
+    pdf.set_font("Arial", "B", 12)
+    pdf.cell(200, 10, txt=f"Dataset: {dataset_name}", ln=True, align="L")
+    pdf.cell(200, 10, txt=f"Number of Columns: {num_cols}", ln=True, align="L")
+    pdf.cell(200, 10, txt="Columns/Variables:", ln=True, align="L")
+    col_names_str = ", ".join(col_names)
+
+    # format
+    pdf.set_font("Arial", size=10)
+    pdf.multi_cell(0, 10, txt=col_names_str)
+
+    # summary stats
+    pdf.set_font("Arial", "B", 12)
+    pdf.cell(200, 10, txt="Summary Statistics:", ln=True, align="L")
+    pdf.image("resources/stats_df.png", x=10, y=None, w=180)
+
+    # data viz
+    pdf.set_font("Arial", "B", 12)
+    pdf.cell(200, 10, txt="Visualizations:", ln=True, align="L")
+    pdf.image("resources/plot.png", x=10, y=None, w=180)
+
+    # save report as pdf
+    pdf_file = "spotify_report.pdf"
+    pdf.output(pdf_file)
+
+    print(f"Summary report written to {pdf_file}")
+
+def main():
+    spotify = read_data()
+    calc_stats(spotify)
+    create_viz(spotify)
+    create_report("data/spotify.csv")
+
+if __name__ == "__main__":
+    main()
---
+++ test_main.py
@@ -0,0 +1,83 @@
+import os
+import polars as pl
+import pandas as pd
+from main import read_data, calc_stats, create_viz, create_report
+
+
+def test_read_data():
+    df = read_data()
+    # test that result is a df
+    assert isinstance(df, pl.DataFrame) and not df.is_empty(), "Error reading data"
+    print("Data loaded successfully.")
+
+
+def test_calc_stats():
+    # creating test df
+    data = {
+        "popularity": [5, 10, 15, 20],
+        "duration_s": [120, 240, 360, 480],
+        "explicit": [0, 1, 0, 1],
+        "danceability": [0.5, 0.6, 0.7, 0.8],
+        "energy": [0.3, 0.6, 0.9, 0.6],
+        "key": [1, 2, 3, 4],
+        "loudness": [-5, -6, -7, -8],
+        "mode": [1, 0, 1, 0],
+        "speechiness": [0.04, 0.05, 0.06, 0.07],
+        "acousticness": [0.1, 0.2, 0.3, 0.4],
+        "instrumentalness": [0.0, 0.1, 0.2, 0.3],
+        "liveness": [0.2, 0.3, 0.4, 0.5],
+        "valence": [0.3, 0.4, 0.5, 0.6],
+        "tempo": [100, 110, 120, 130],
+        "time_signature": [4, 4, 3, 3],
+    }
+    df = pl.DataFrame(data)
+
+    # call calc_stats function
+    stats_df = calc_stats(df)
+    pl.Config.set_tbl_rows(50)
+    print(stats_df)
+
+    # assert function with polars
+    def assert_value(column, metric, expected_value, tolerance=0.01):
+        # Get the value for the specified column and metric
+        actual_value = (
+            stats_df.filter(pl.col("column") == column)
+            .select(pl.col(metric))
+            .to_numpy()[0][0]
+        )
+        assert (
+            abs(actual_value - expected_value) <= tolerance
+        ), f"{metric} for {column} does not match. Expected: {expected_value}, Got: {actual_value}"
+
+    # run assertions
+    assert_value("popularity", "mean", 12.50)
+    assert_value("duration_s", "median", 300.00)
+    assert_value("energy", "std_dev", 0.24)
+    assert_value("key", "mean", 2.50)
+    assert_value("loudness", "median", -6.50)
+    assert_value("tempo", "std_dev", 12.91)
+
+    print("All assertions passed for calc_stats.")
+
+
+def test_create_viz():
+    df = read_data()
+    create_viz(df)
+
+    # verify file was created and not empty
+    assert os.path.exists("resources/plot.png"), "The plot.png file does not exist."
+    assert os.path.getsize("resources/plot.png") > 0, "The plot.png file is empty."
+
+    print("All assertions passed for test_create_viz.")
+
+
+def test_generate_report():
+    create_report("data/spotify.csv")
+
+
+if __name__ == "__main__":
+    test_read_data()
+    test_calc_stats()
+    test_create_viz()
+    test_generate_report()
+    # testing ci/cd for report generation
